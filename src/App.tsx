2b39d73581f93237c8c51c1f2bba6683338ea3cf
ibulavsky <ibulavsky@gmail.com>
2020-01-17 17:57:09 +0300

    Added Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Main from "./neko-1-main/main-1-ui/Main";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./neko-1-main/main-2-bll/store";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Provider store={store}>
+                    <Main/>
+                </Provider>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
